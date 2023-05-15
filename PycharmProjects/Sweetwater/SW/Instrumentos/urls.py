abe8f9104e5548679a5c37d7e0d77da8617571f2
Saul Salazar Ozuna <saulsalazar2001@outlook.com>
2023-05-15 15:17:18 -0700

    [Servidor y base]

--- PycharmProjects/Sweetwater/SW/Instrumentos/urls.py
+++ PycharmProjects/Sweetwater/SW/Instrumentos/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-
+    
+    path("", views.Guitarra, name="Guitarra"),
+path("Registro", views.RV, name="Registro"),
 ]
--- PycharmProjects/Sweetwater/SW/Instrumentos/views.py
+++ PycharmProjects/Sweetwater/SW/Instrumentos/views.py
@@ -7,6 +7,22 @@ from django.http import HttpResponse, JsonResponse
 def index(request):
     Ventas = ventas.objects.all()
     context = {
-        "ventas":ventas
+        "ventas":Ventas
     }
     return render(request,"Index.html", context)
+
+def Guitarra(request):
+    Ventas = ventas.objects.all()
+    context = {
+        "Ventas":Ventas
+    }
+    return render(request,"Guitarra.html", context)
+
+def RV(request):
+    if request.method == 'POST':
+        Ventas = ventas()
+        Ventas.idventa = request.POST.get('ID')
+        Ventas.Total = request.POST.get('Total')
+        Ventas.Fecha = request.POST.get('Fecha')
+        Ventas.save()
+        return JsonResponse(["Listo"], safe=False)
